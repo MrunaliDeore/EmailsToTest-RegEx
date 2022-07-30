42a3852e5789f9548c62e479b350401fb5c1ebe2
MrunaliDeore <mrunalideoreohk1@gmail.com>
2022-07-30 17:10:31 +0530

    Added : valid first name min 3 char

---
+++ ClassRegExTest.java
@@ -0,0 +1,17 @@
+package Junit;
+
+import static org.junit.Assert.*;
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class ClassRegExTest {
+
+	@Test
+	public void firstnameTest() {
+		ClassRefEx ur = new ClassRefEx();
+		boolean result = ur.firstname("Mrunali");
+		Assert.assertEquals(true, result);
+	}
+
+}
