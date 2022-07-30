8587e82e5d42d73d5670b235e8446daec0fc04ce
MrunaliDeore <mrunalideoreohk1@gmail.com>
2022-07-30 17:07:43 +0530

    Added ; added main class file

---
+++ MainClass.java
@@ -0,0 +1,10 @@
+package Junit;
+
+public class MainClass {
+	public static void main(String[] args){
+		System.out.println("Assignments -20\n");
+		System.out.println("User RegistrationProblem- Using JUNIT");
+		
+	}
+
+}
